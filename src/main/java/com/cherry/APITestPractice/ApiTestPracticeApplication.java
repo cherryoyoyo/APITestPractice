2600a8822ae2f626f322beea05fdd99c5c100dc7
cherryhaung <shps950421@gmail.com>
2025-03-05 22:17:01 +0800

    init

---
+++ src/main/java/com/cherry/APITestPractice/ApiTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.APITestPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/APITestPractice/ApiTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.APITestPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
